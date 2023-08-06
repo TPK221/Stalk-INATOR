b9c61883989decd9a11a8b28a4efcd7fe8c4248f
Tripti Pal Kaur <triptipalkaur@gmail.com>
2023-08-06 23:25:25 +0800

    edit again

--- resources/views/student/studentdash.blade.php
+++ resources/views/student/studentdash.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="mdk-header-layout__content page-content pb-0">
-    <div class="py-54pt bg-gradient-primary">
+    <div class="py-32pt bg-gradient-primary">
         <div class="container d-flex flex-column flex-md-row align-items-center text-center text-md-left">
             <div class="flex mb-32pt mb-md-0">
                 <h1 class="text-white mb-8pt">Student Dashboard</h1>
@@ -10,69 +10,38 @@
         </div>
     </div>
 </div>
+
 <br><br>
-<div class="container page__container page-section p-0">
-    <div class="card shadow-lg">
-        <div class="card-body p-5">
-            <div class="row align-items-center">
-                <div class="col-md-4">
-                    <div class="text-center">
-                        <img class="img-view-profile" src="/storage/images/{{auth()->user()->studentProfile->image ?? 'default-image.jpg'}}" alt="Profile Image">
-                    </div>
-                </div>
-                <div class="col-md-8">
-                    <div class="container pt-4">
-                        @if(Auth::user()->hasRole('student'))
-                            @if(isset(auth()->user()->studentProfile->fullName))
-                            <span class="pl-5 font-weight-bold">Name : </span><span>{{ auth()->user()->studentProfile->fullName }}</span><br>
-                        @else
-                            <span class="pl-5 font-weight-bold">Name : </span><span>-</span><br>
-                        @endif
+<div class="container page__container page-section border border-dark rounded shadow-lg p-5 mb-5 bg-white ">
 
-                        <span class="pl-5 font-weight-bold ">Username : </span><span class="">{{ Auth::user()->name }}</span><br>
-                        <span class="pl-5 font-weight-bold ">Email : </span><span class="">{{ Auth::user()->email }}</span><br>
+<img class="img-view-profile" src="/storage/images/{{auth()->user()->studentProfile->image ?? 'default-image.jpg'}}" alt="Profile Image">
+    <div class="container pt-5 container-text-student">
+        @if(Auth::user()->hasRole('student'))
 
-                            @if(isset(auth()->user()->studentProfile->studentID))
-                            <span class="pl-5 font-weight-bold">Student ID : </span><span>{{ auth()->user()->studentProfile->studentID }}</span><br> 
-                            @else
-                            <span class="pl-5 font-weight-bold">Lecturer ID : </span><span>-</span><br>
-                            @endif
-                        @endif
-                    </div>
-                </div>
-            </div>
-        </div>
+        @if(isset(auth()->user()->studentProfile->fullName))
+        <span class="pl-5 font-weight-bold">Name : </span><span>{{ auth()->user()->studentProfile->fullName }}</span><br>
+        @else
+        <span class="pl-5 font-weight-bold">Name : </span><span>-</span><br>
+        @endif
+
+        <span class="pl-5 font-weight-bold ">Username : </span><span class="">{{ Auth::user()->name }}</span><br>
+        <span class="pl-5 font-weight-bold ">Email : </span><span class="">{{ Auth::user()->email }}</span><br>
+
+        @if(isset(auth()->user()->studentProfile->studentID))
+        <span class="pl-5 font-weight-bold">Student ID : </span><span>{{ auth()->user()->studentProfile->studentID }}</span><br>
+        @else
+        <span class="pl-5 font-weight-bold">Student ID : </span><span>-</span><br>
+        @endif
+
+        @endif
+        <br>
     </div>
+
 </div>
 
 <br>
 <br>
 <br>
-<br><br>
 
-@endsection
 
-@section('style')
-<style>
-    .img-view-profile {
-        width: 200px;
-        height: 200px;
-        border-radius: 50%;
-        object-fit: cover;
-    }
-
-    .font-weight-bold {
-        font-weight: bold;
-    }
-
-    .card {
-        border: none;
-        border-radius: 8px;
-        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.08);
-    }
-
-    .card-body {
-        padding: 40px;
-    }
-</style>
 @endsection
